03c940d05ae6558c7ca3c4370f94dd63e68cfe45
Arthemises <coralie.allioux@gmail.com>
2021-11-07 18:34:42 +0100

    Adding compile file only for DLX

---
+++ DLX_vhd/compile_DLX.bash
@@ -0,0 +1,14 @@
+vcom 000-globals.vhd
+vcom a-Log2.vhd
+vcom a.a-CU_HW.vhd
+vcom a.c-IRAM.vhd
+vcom a.b-DataPath.core/a.b.a-ALU.vhd
+vcom a.b-DataPath.core/a.b.c-adder.vhd
+vcom a.b-DataPath.core/a.b.c-extender.vhd
+vcom a.b-DataPath.core/a.b.c-mem.vhd
+vcom a.b-DataPath.core/a.b.c-mux21.vhd
+vcom a.b-DataPath.core/a.b.c-register.vhd
+vcom a.b-DataPath.core/a.b.c-registerfile.vhd
+vcom a.b-DataPath.vhd
+vcom a-DLX.vhd
+vcom test_bench/TB_DLX.vhd
