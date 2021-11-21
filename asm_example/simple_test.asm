ffa1ac9c3339c4094bc0a5d40a02687736fccfb8
Arthemises <coralie.allioux@gmail.com>
2021-11-21 15:20:57 +0100

    Updating asm test file

--- asm_example/simple_test.asm
+++ asm_example/simple_test.asm
@@ -1,6 +1,11 @@
 addi r1,r0,#2
+addi r2,r0,#-2
+addi r3,r0,#6
+addi r4,r0,#-10
+sw 10(r0),r1
+addi r31,r0,#128
 subi r2,r1,#1
-addi r3,r1,#-4
 subi r4,r3,#-1
-add r5,r1,r2
-sub r6,r3,r4
+add r1,r3,r1
+sub r3,r0,r31
+lw r20, 10(r0)
