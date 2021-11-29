4d648fbe3f734382d6cc3c9ed5f18843a6a9c95e
Arthemises <coralie.allioux@gmail.com>
2021-11-29 15:18:15 +0100

    Adding possibility to choose between signed and unsigned

--- DLX_vhd/a.b-DataPath.core/a.b.c-extender.vhd
+++ DLX_vhd/a.b-DataPath.core/a.b.c-extender.vhd
@@ -11,10 +11,11 @@ use IEEE.numeric_std.all;
 use work.myTypes.all;
 
 entity EXTENDER is
-  generic (	N : integer := 32;
-			IMM_field_lenght : integer := 16);
-  port   ( NOT_EXT_IMM: IN std_logic_vector(IMM_field_lenght-1 downto 0); -- input data
-           EXT_IMM: OUT std_logic_vector(N-1 downto 0)); -- output data(i.e. input data with sign extension)
+  generic (N : integer := 32;
+			     IMM_field_lenght : integer := 16);
+  port   ( NOT_EXT_IMM:     IN std_logic_vector(IMM_field_lenght-1 downto 0); -- input data
+           SIGNED_IMM:      IN std_logic;
+           EXT_IMM:         OUT std_logic_vector(N-1 downto 0)); -- output data(i.e. input data with sign extension)
 end EXTENDER;
 
 architecture BEHAVIOR of EXTENDER is
@@ -24,7 +25,11 @@ begin
 -- process that extends the sign of every input data
 EXTENSION: process (NOT_EXT_IMM)
   begin
-	EXT_IMM <= std_logic_vector(resize(signed(NOT_EXT_IMM), N));
+  if SIGNED_IMM='1' then
+	  EXT_IMM <= std_logic_vector(resize(signed(NOT_EXT_IMM), N));
+  else
+	  EXT_IMM <= std_logic_vector(resize(unsigned(NOT_EXT_IMM), N));
+  end if;
   end process EXTENSION;
 
 end BEHAVIOR;
