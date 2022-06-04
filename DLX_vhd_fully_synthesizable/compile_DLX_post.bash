ec57e9c1009490aff83c2913818d33205ea58a9c
Arthemises <coralie.allioux@gmail.com>
2022-06-04 18:47:47 +0200

    Update TB and compile file for post-syn sim

---
+++ DLX_vhd_fully_synthesizable/compile_DLX_post.bash
@@ -0,0 +1,6 @@
+vcom 000-Log2.vhd
+vcom 001-globals.vhd
+vcom a.c-IRAM.vhd
+vcom a.b-DataPath.core/a.b.g-mem.vhd
+vcom DLX_postsynth_simple.vhdl
+vcom test_bench/TB_DLX.vhd
--- DLX_vhd_fully_synthesizable/test_bench/TB_DLX.vhd
+++ DLX_vhd_fully_synthesizable/test_bench/TB_DLX.vhd
@@ -42,9 +42,6 @@ architecture TEST of tb_dlx is
     end component;
 
     component DLX
-	generic(N : integer := WORD;
-		DRAM_SIZE : integer := DRAM_SIZE;
-		IR_SIZE : integer := IR_SIZE);
 	port(	CLK: IN std_logic;
 		RST: IN std_logic;
 
@@ -56,8 +53,8 @@ architecture TEST of tb_dlx is
 		D_RR: OUT std_logic;
 		D_WR: OUT std_logic;
 		D_ADDR: OUT std_logic_vector(Log2(DRAM_SIZE)-1 downto 0);
-		D_DATA_IN: OUT std_logic_vector(N-1 downto 0);
-		D_DATA_OUT: IN std_logic_vector(N-1 downto 0));
+		D_DATA_IN: OUT std_logic_vector(WORD-1 downto 0);
+		D_DATA_OUT: IN std_logic_vector(WORD-1 downto 0));
     end component;
 begin
 
@@ -78,7 +75,7 @@ begin
 	end process;
 	
 	--Reset <= '0', '1' after 6 ns, '0' after 11 ns, '1' after 15 ns;
-	Reset <= '0', '1' after 6 ns;
+	Reset <= '1', '0' after 2 ns, '1' after 6 ns;
        
 
 end TEST;
