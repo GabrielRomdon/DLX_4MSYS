3beb36ab094b91e9b963747c1ac2cfd916916e87
Arthemises <coralie.allioux@gmail.com>
2022-06-12 12:32:33 +0200

    Update UVM test assembly, more coverage

--- asm_example/UVM_test.asm
+++ asm_example/UVM_test.asm
@@ -1,3 +1,4 @@
+init:
 addi r1,r0,#2
 ori r2,r0,#7
 slei r3,r0,#6
@@ -10,12 +11,22 @@ srli r2,r2,#1
 xori r4,r3,#45000
 addi r16,r0,#4500
 add r1,r3,r1
-j label
+bnez r2, label
 nop
 nop
 nop
+jal end
 nop
-sub r3,r0,r31
+nop
+nop
+not done:
+sub r6,r4,r31
+sw 16(r0), r16
+end:
+and r6, r4, r16
 slli r31,r31,#2
 lw r20, 10(r0)
-
+j init
+nop
+nop
+nop
